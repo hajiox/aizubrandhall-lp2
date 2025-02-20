87e7dfff01add9d5293304b9f6e1004f54322171
hajiox <ts@aizu-tv.com>
2025-02-20 17:26:13 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 }
 
 module.exports = nextConfig
