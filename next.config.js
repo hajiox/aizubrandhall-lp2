14213c39230e37e5c3a9dfa7a5dbd875a6baa91b
hajiox <ts@aizu-tv.com>
2025-02-20 17:21:26 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: true,
-  swcMinify: true,
-  poweredByHeader: false,
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 module.exports = nextConfig
